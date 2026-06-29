ef8686e36f588054409a85e7b76b7e01a69d5b36
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-06-29 10:38:40 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,15 +4,25 @@ set -eu
 
 ARCH=$(uname -m)
 
-echo "Installing package dependencies..."
-echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
-
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
+echo "Building package dependencies and package..."
+echo "---------------------------------------------------------------"
+make-aur-package python-ollama
+make-aur-package python-newspaper
+make-aur-package python-speechrecognition
+make-aur-package python-openwakeword
+make-aur-package llama.cpp
+make-aur-package python-llama-index-core
+make-aur-package python-model2vec
+make-aur-package python-tokenizers
+make-aur-package faiss-cpu
+make-aur-package python-ddgs
+make-aur-package python-fake-useragent
 make-aur-package
+
 
 # If the application needs to be manually built that has to be done down here
 
