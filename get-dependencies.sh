73d99a55970ccbd9a1d4bec3dc4fb110b21a52dd
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-06-29 12:25:16 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,8 +12,12 @@ echo "Building package dependencies and package..."
 echo "---------------------------------------------------------------"
 make-aur-package python-ollama
 make-aur-package python-newspaper
+# python-sounddevice is a dependency to pocketsphinx
 make-aur-package python-sounddevice && make-aur-package pocketsphinx
+# python-proto-plus is a dependency to python-grpcio-status
 make-aur-package python-proto-plus && make-aur-package python-grpcio-status
+# python-grpcio-status and python-proto-plus are a dependency to python-google-cloud-speech,
+# python-google-cloud-speech and python-groq are a dependency to python-speechrecognition
 make-aur-package python-groq && make-aur-package python-google-cloud-speech && make-aur-package python-speechrecognition
 make-aur-package python-openwakeword
 make-aur-package llama.cpp
