b45582420891a427b693234d11c93fdc5ba14dc2
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-06-29 11:57:32 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,6 +12,9 @@ echo "Building package dependencies and package..."
 echo "---------------------------------------------------------------"
 make-aur-package python-ollama
 make-aur-package python-newspaper
+make-aur-package python-pocketsphinx
+make-aur-package python-groq
+make-aur-package python-google-cloud-speech
 make-aur-package python-speechrecognition
 make-aur-package python-openwakeword
 make-aur-package llama.cpp
