6167514189b07c9ca20e7835bd0c4e2e33f5dcc8
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-06-29 12:43:53 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,11 +21,13 @@ make-aur-package python-proto-plus && make-aur-package python-grpcio-status
 make-aur-package python-groq && make-aur-package python-google-cloud-speech && make-aur-package python-speechrecognition
 # python-tflite-runtime is a dependency to python-openwakeword
 make-aur-package python-tflite-runtime && make-aur-package python-openwakeword
-make-aur-package llama.cpp
-make-aur-package python-llama-index-core
-make-aur-package python-model2vec
-make-aur-package python-tokenizers
+# python-gguf is a dependency to llama.cpp
+make-aur-package python-gguf && make-aur-package llama.cpp
+# python-banks, python-dataclasses-json, python-llama-index-instrumentation and python-llama-index-workflows are dependencies to python-llama-index-core
+make-aur-package python-banks && make-aur-package python-dataclasses-json && make-aur-package python-llama-index-instrumentation && make-aur-package python-llama-index-workflows && make-aur-package python-llama-index-core
+# python-tokenizers is a dependency to python-model2vec
+make-aur-package python-tokenizers && make-aur-package python-model2vec
 make-aur-package faiss-cpu
-make-aur-package python-ddgs
-make-aur-package python-fake-useragent
+# python-fake-useragent and python-primp are dependencies to python-ddgs
+make-aur-package python-fake-useragent && make-aur-package python-primp && make-aur-package python-ddgs
 make-aur-package
